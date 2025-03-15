636c1f400ab8402d55ce1b47e37c16de2291a754
Paratha-iscool <prarthna.isthebest@gmail.com>
2025-03-15 17:57:49 +0400

    Main screen finished

--- scenes/main_screen.gd
+++ scenes/main_screen.gd
@@ -1,1 +1,20 @@
 extends Node2D
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/todo.tscn")
+
+
+func _on_alters_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/todo.tscn")
+
+
+func _on_info_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/todo.tscn")
+
+
+func _on_timeline_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/todo.tscn")
+
+
+func _on_talk_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/todo.tscn")
